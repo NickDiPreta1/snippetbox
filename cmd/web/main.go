bdffb1b5def653e4fd93ccfc6bc5d9ac0caf2297
Nicholas DiPreta <nicholasdipreta@Nicholass-MacBook-Pro.local>
2025-11-02 14:48:30 -0500

    feat: style and more templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,22 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// FileServer takes a file system as an argument
+	// so we can change http.Dir for something more dynamic
+	// like S3 in the future.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")
-
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
