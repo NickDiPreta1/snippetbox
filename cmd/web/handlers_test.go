6a2cca02936f072b3d24ce1f921a290fb3f9cc0b
Nicholas DiPreta <nicholasdipreta@gmail.com>
2025-12-03 17:10:33 -0500

    feat: testing through 354

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,32 +1,19 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/NickDiPreta1/snippetbox/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := newTestApplication()
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	ts := newTestServer(t, app.routes())
 
-	ping(rr, r)
+	statusCode, _, body := ts.get(t, "/ping")
 
-	res := rr.Result()
-	assert.Equal(t, res.StatusCode, http.StatusOK)
-
-	resBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	assert.Equal(t, string(resBytes), "OK")
+	assert.Equal(t, statusCode, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
