2b600ed2bb042d08c4b0321ca8dda7ef096d0d80
Nicholas DiPreta <nicholasdipreta@gmail.com>
2025-12-10 20:44:54 -0500

    feat: user account page finished

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -232,5 +232,9 @@ func (app *application) accountView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	fmt.Fprintf(w, "%+v", user)
+
+	data := app.newTemplateData(r)
+	data.User = user
+
+	app.render(w, http.StatusOK, "account.tmpl.html", data)
 }
