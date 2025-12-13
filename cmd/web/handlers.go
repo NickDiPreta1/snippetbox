9a8a1a5619c4cca9969e0aa278b5eedf2584e8e1
Nicholas DiPreta <nicholasdipreta@gmail.com>
2025-12-13 13:01:18 -0500

    feat: accountPasswordUpdatePost validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -246,9 +246,9 @@ func (app *application) accountView(w http.ResponseWriter, r *http.Request) {
 }
 
 type changePasswordForm struct {
-	currentPassword         string `form:"currentPassword"`
-	newPassword             string `form:"newPassword"`
-	newPasswordConfirmation string `form:"newPasswordConfirmation"`
+	CurrentPassword         string `form:"currentPassword"`
+	NewPassword             string `form:"newPassword"`
+	NewPasswordConfirmation string `form:"newPasswordConfirmation"`
 	validator.Validator     `form: "-"`
 }
 
@@ -259,5 +259,25 @@ func (app *application) accountPasswordUpdate(w http.ResponseWriter, r *http.Req
 }
 
 func (app *application) accountPasswordUpdatePost(w http.ResponseWriter, r *http.Request) {
+	var form changePasswordForm
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form.CheckField(validator.NotBlank(form.CurrentPassword), "currentPassword", "This field cannot be blank")
+	form.CheckField(validator.NotBlank(form.NewPassword), "newPassword", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.NewPassword, 8), "newPassword", "This field must be at least 8 characters long")
+	form.CheckField(validator.NotBlank(form.NewPasswordConfirmation), "newPasswordConfirmation", "This field cannot be blank")
+	form.CheckField(form.NewPassword == form.NewPasswordConfirmation, "newPasswordConfirmation", "Passwords do not match")
+
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+
+		app.render(w, http.StatusUnprocessableEntity, "password.tmpl.html", data)
+		return
+	}
 
 }
